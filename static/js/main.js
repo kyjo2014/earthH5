f47db19ae5935a13157c69a89f78628b8ea460dd
kyjo2014 <467346087@qq.com>
2017-05-29 19:06:24 +0800

    增加云层

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,3 @@
-function bind() {
-
-}
 
 
 /**
@@ -11,15 +8,15 @@ class render3D {
     constructor() {
         this.render = this.render.bind(this);
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-        this.camera.position.z = 160;
+        this.camera = new THREE.PerspectiveCamera(45, 800 / 1600, 1, 1000);
+        this.camera.position.z = 200;
         this.renderer = new THREE.WebGLRenderer({
             alpha: true,
             canvas: document.querySelector('#stage')
         })
         this.renderer.setClearColor(0xffffff, 0.6)
         this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setSize(800, 1600);
         this.renderer.render(this.scene, this.camera)
     }
     addEarth() {
@@ -35,11 +32,14 @@ class render3D {
             specularMap: earth_specular
         });
         this.earth = new THREE.Mesh(earth_geometry, earth_material);
+        this.earth.scale.x = 0.6
+        this.earth.scale.y = 0.6
+        this.earth.scale.z = 0.6
         this.scene.add(this.earth);
     }
     addCloud() {
-        let cloud_texture = new THREE.TextureLoader().load('tx/cloud.png');
-        let cloud_geometry = new THREE.SphereGeometry(31, 32, 32);
+        let cloud_texture = new THREE.TextureLoader().load('./static/texture/cloud.png');
+        let cloud_geometry = new THREE.SphereGeometry(32,32,32);
         let cloud_material = new THREE.MeshBasicMaterial({
             shininess: 10,
             map: cloud_texture,
@@ -47,30 +47,58 @@ class render3D {
             opacity: 0.8
         });
         this.cloud = new THREE.Mesh(cloud_geometry, cloud_material);
-        this.scene.add(cloud);
+        this.cloud.scale.x = 0.6
+        this.cloud.scale.y = 0.6
+        this.cloud.scale.z = 0.6
+        this.scene.add(this.cloud);
     }
     addLight() {
-        let light = new THREE.DirectionalLight(0xffffff, 0.1)
-        light.position.x = -150
-        light.position.y = 150
-        light.position.z = 20
-       var ambientLight = new THREE.AmbientLight(0xffffff);
+        let dirlight = new THREE.DirectionalLight(0xffffff, 0.4)
+        dirlight.position.x = -150
+        dirlight.position.y = 150
+        dirlight.position.z = 0
+        var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
         this.scene.add(ambientLight);
-        this.scene.add(light)
+        this.scene.add(dirlight)
     }
     addEvent() {
-
+    
     }
     render() {
         requestAnimationFrame(this.render);
         // stats.begin();
         this.earth.rotation.y += 0.001;
-        // cloud.rotation.y += 0.001;
+        this.cloud.rotation.y += 0.002;
         // stats.end();
         this.renderer.render(this.scene, this.camera);
     }
+    rotateScene() {
+        this.camera.rotation.x += 0.001
+        
+    }
+    rotateEarth(deg) {
+        this.earth.rotation.y += deltaX / 300;
+        this.earth.rotation.x += deltaY / 300;
+    }
+    rotateCloud(deg) {
+        this.cloud.rotation.y += deltaX / 300;
+        this.cloud.rotation.x += deltaY / 300;
+    }
+    addPoint() {   
+
+    }
+    checkAim() {
+        
+    }
 
 
+
+}
+
+
+
+
+class App {
 
 }
 
@@ -84,6 +112,12 @@ function initStage() {
     let stage = new render3D()
     stage.addEarth()
     stage.addLight()
+    stage.addCloud()
     stage.render()
+
+}
+
+
+function bindEvent() {
 
 }
