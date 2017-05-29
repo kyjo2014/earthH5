093b284469ef14a0fdc7cf36df3d951925a22b1c
kyjo2014 <467346087@qq.com>
2017-05-29 23:39:15 +0800

    点的显示完成

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,31 @@
-
-
 /**
  * canvas渲染
  * 
  */
 class render3D {
-    constructor() {
+    constructor(opt) {
+        if (opt)
+            this.proxy(opt)
         this.render = this.render.bind(this);
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(45, 800 / 1600, 1, 1000);
-        this.camera.position.z = 200;
+        this.camera.position.z = 328;
         this.renderer = new THREE.WebGLRenderer({
             alpha: true,
             canvas: document.querySelector('#stage')
         })
-        this.renderer.setClearColor(0xffffff, 0.6)
+        this.renderer.setClearColor(0xffffff, 0)
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(800, 1600);
         this.renderer.render(this.scene, this.camera)
     }
+    proxy(opt) {
+        for (let i in opt) {
+            this[`_${i}`] = opt[i]
+        }
+    }
     addEarth() {
-        let earth_geometry = new THREE.SphereGeometry(31, 32, 32)
+        let earth_geometry = new THREE.SphereGeometry(20, 40, 40)
         let earth_texture = new THREE.TextureLoader().load("./static/texture/earth.jpeg");
         let earth_bump = new THREE.TextureLoader().load("./static/texture/bump.jpeg");
         let earth_specular = new THREE.TextureLoader().load("./static/texture/spec.jpeg");
@@ -32,14 +37,11 @@ class render3D {
             specularMap: earth_specular
         });
         this.earth = new THREE.Mesh(earth_geometry, earth_material);
-        this.earth.scale.x = 0.6
-        this.earth.scale.y = 0.6
-        this.earth.scale.z = 0.6
         this.scene.add(this.earth);
     }
     addCloud() {
         let cloud_texture = new THREE.TextureLoader().load('./static/texture/cloud.png');
-        let cloud_geometry = new THREE.SphereGeometry(32,32,32);
+        let cloud_geometry = new THREE.SphereGeometry(21, 32, 32);
         let cloud_material = new THREE.MeshBasicMaterial({
             shininess: 10,
             map: cloud_texture,
@@ -47,9 +49,6 @@ class render3D {
             opacity: 0.8
         });
         this.cloud = new THREE.Mesh(cloud_geometry, cloud_material);
-        this.cloud.scale.x = 0.6
-        this.cloud.scale.y = 0.6
-        this.cloud.scale.z = 0.6
         this.scene.add(this.cloud);
     }
     addLight() {
@@ -62,19 +61,20 @@ class render3D {
         this.scene.add(dirlight)
     }
     addEvent() {
-    
+
     }
     render() {
         requestAnimationFrame(this.render);
         // stats.begin();
-        this.earth.rotation.y += 0.001;
-        this.cloud.rotation.y += 0.002;
+
+        this.rotateScene()
         // stats.end();
         this.renderer.render(this.scene, this.camera);
     }
     rotateScene() {
-        this.camera.rotation.x += 0.001
-        
+        this.earth.rotation.y += 0.005;
+        this.cloud.rotation.y += 0.002;
+        this.particles.rotation.y += 0.005;
     }
     rotateEarth(deg) {
         this.earth.rotation.y += deltaX / 300;
@@ -84,11 +84,66 @@ class render3D {
         this.cloud.rotation.y += deltaX / 300;
         this.cloud.rotation.x += deltaY / 300;
     }
-    addPoint() {   
+    addPoint(points) {
+        let pointGeo = new THREE.Geometry()
+        let sprite = new THREE.TextureLoader().load("./static/texture/sprite/i_galapagos.png");
 
+        if (this._places || points) {
+            let len = points.length
+            let place = this._places || points
+            for (let i = 0; i < len; i++) {
+                let positon = this.getPosition(place[i].lng, place[i].lat, 30)
+                let vertex = new THREE.Vector3(positon.x, positon.y, positon.z)
+                pointGeo.vertices.push(vertex)
+            }
+        }
+        let material = new THREE.PointsMaterial({
+            size: 30,
+            sizeAttenuation: true,
+            map: sprite,
+            transparent: true,
+            precision: "highp"
+        });
+        this.particles = new THREE.Points(pointGeo, material);
+        // this.particles.
+        // this.particles.scale.set(0.3, 0.3, 0.3)
+        this.scene.add(this.particles);
     }
     checkAim() {
-        
+
+    }
+    testGeo() {
+        let testSphGeo = new THREE.SphereGeometry(33, 5, 5)
+        let sprite = new THREE.TextureLoader().load("./static/texture/sprite/i_galapagos.png");
+        let material = new THREE.PointsMaterial({
+            size: 90,
+            sizeAttenuation: false,
+            map: sprite,
+            alphaTest: 0.5,
+            transparent: false
+        });
+
+        // material.color.setHSL(1.0, 0.3, 0.7);
+        let particles = new THREE.Points(testSphGeo, material);
+        console.log(testSphGeo.vertices)
+        particles.scale.x = 0.6
+        particles.scale.y = 0.6
+        particles.scale.z = 0.6
+        this.scene.add(particles);
+    }
+
+    getPosition(lng, lat, alt) {
+        var phi = (90 - lat) * (Math.PI / 180),
+            theta = (lng + 180) * (Math.PI / 180),
+            radius = alt,
+            x = -(radius * Math.sin(phi) * Math.cos(theta)),
+            z = (radius * Math.sin(phi) * Math.sin(theta)),
+            y = (radius * Math.cos(phi));
+        return {
+            x: x,
+            y: y,
+            z: z
+        };
     }
 
 
@@ -113,6 +168,14 @@ function initStage() {
     stage.addEarth()
     stage.addLight()
     stage.addCloud()
+    stage.addPoint([{
+        lng: 113.257291,
+        lat: 23.149415
+    }, {
+        lng: 116.427915,
+        lat: 39.902895
+    }])
+    // stage.testGeo()
     stage.render()
 
 }
